5c3a4e8ce45bb4512ae2755f84fa481cbf76cfb2
chenwuyang <chenwuyang1@huawei.com>
2026-05-18 11:20:21 +0800

    debug hacks
    Signed-off-by: chenwuyang <chenwuyang1@huawei.com>

--- third_party/ascend/lib/DynamicCVPipeline/AddDynamicCVPipeline.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AddDynamicCVPipeline.cpp
@@ -20,22 +20,20 @@
  * THE SOFTWARE.
  */
 
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/Support/Debug.h"
+#include <cstdint>
+#include <optional>
 
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/LogicalResult.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Diagnostics.h"
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Support/WalkResult.h"
-#include "llvm/Support/Debug.h"
-#include <cstdint>
-#include <optional>
 
 #include "ascend/include/DynamicCVPipeline/AddControlFlowCondition.h"
 #include "ascend/include/DynamicCVPipeline/AllocMultiCache.h"
@@ -52,6 +50,7 @@
 #include "ascend/include/DynamicCVPipeline/StandardizeOp.h"
 
 #include "DynamicCVPipeline/Common/FallbackHelper.h"
+#include "triton/Tools/Sys/GetEnv.hpp"
 
 static constexpr const char *DEBUG_TYPE = "add-dynamic-cv-pipeline";
 static constexpr unsigned MAX_RETRY_TIMES = 2;
@@ -149,8 +148,11 @@ void AddDynamicCVPipelinePass::runOnOperation() {
     // Do not reuse pass instances or partially transformed IR on retry.
     PassManager pm(&getContext(), moduleOp.getOperationName());
     if (failed(mlir::applyPassManagerCLOptions(pm))) {
-      signalPassFailure();
-      return;
+      LDBG("Failed to apply cli options - running in python");
+    }
+
+    if (tools::getBoolEnv("MLIR_ENABLE_DUMP")) {
+      pm.enableIRPrinting();
     }
 
     addPasses(pm);
@@ -159,6 +161,7 @@ void AddDynamicCVPipelinePass::runOnOperation() {
     auto result = pm.run(moduleOp);
     auto errCode = getErrorCode(moduleOp);
     if (succeeded(result) && !errCode.has_value()) {
+      checkAndDisableVfSub(moduleOp);
       LDBG("Process successfully");
       return;
     }
@@ -201,7 +204,6 @@ void AddDynamicCVPipelinePass::runOnOperation() {
     return;
   }
 
-  checkAndDisableVfSub(moduleOp);
   LDBG("Process successfully");
 }
 
