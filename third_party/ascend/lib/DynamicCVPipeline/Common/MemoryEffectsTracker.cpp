73390e4564475f5da8df9036a19a4a5e09895d29
olivervnc <chenwuyang1@huawei.com>
2026-09-22 11:05:19 +0800

    Revert "[ssbuffer](fix) memref generators no longer considered as writer (#2168)"
    This reverts commit e2c0a8ded3cecdd60f034dabb547ac0985b0d2af.

--- third_party/ascend/lib/DynamicCVPipeline/Common/MemoryEffectsTracker.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/Common/MemoryEffectsTracker.cpp
@@ -253,17 +253,14 @@ void MemoryDependenceGraph::analyzeOp(Operation *op) {
   SmallVector<Operation *> defs;
   SmallVector<Operation *> preds;
   collectPreds(effects, unknown, defs, preds);
-
-  LLVM_DEBUG({
-    LOG_DEBUG("Defs: \n");
-    for (auto def : defs) {
-      LOG_DEBUG(*def << "\n");
-    }
-    LOG_DEBUG("Preds: \n");
-    for (auto pred : preds) {
-      LOG_DEBUG(*pred << "\n");
-    }
-  });
+  LOG_DEBUG("Defs: \n");
+  for (auto def : defs) {
+    LOG_DEBUG(*def << "\n");
+  }
+  LOG_DEBUG("Preds: \n");
+  for (auto pred : preds) {
+    LOG_DEBUG(*pred << "\n");
+  }
 
   // Step 3: extract edges from defs and preds to the graph.
   recordEdges(op, defs, preds);
@@ -511,6 +508,7 @@ void MemoryDependenceGraph::applyEffects(
     if (isa<BaseMemRefType>(result.getType())) {
       if (MemSlot *s = getOrCreateSlot(result)) {
         s->dataSource = op;
+        s->lastWriter = op;
         s->pendingReads.clear();
       }
     }
